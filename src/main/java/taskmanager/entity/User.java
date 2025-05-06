f3f43b7620d7700459cda14d308b96e51a3e4e4b
teo <teo.th@hotmail.gr>
2025-05-06 22:57:28 +0200

    added JsonIgnore to password

--- src/main/java/taskmanager/entity/User.java
+++ src/main/java/taskmanager/entity/User.java
@@ -47,7 +47,7 @@ public class User {
 	@Column(unique = true, nullable = false)
 	private String email;
 	
-	
+	@JsonIgnore
 	@NotBlank(message = "password required")
 	@Size(min = 6, message = "Password must be at least 6 characters long")
 	private String password;
